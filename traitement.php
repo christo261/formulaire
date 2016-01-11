30ac92d4532cb5ac2f3a780b4dfccb9341bf8aa3
CHRIS <cdunan2607@club-internet.fr>
2016-01-11 21:40:43 +0100

    Affichage de page de confirmation d'inscription

---
+++ traitement.php
@@ -0,0 +1,14 @@
+﻿<html>
+
+  <head>
+    <meta charset="utf-8"> 
+   </head>
+<body>
+	<?php
+		$pseudo = $_POST['pseudo'];
+		$mail=$_POST['email'];
+		print("Hello $pseudo, vous êtes bien inscrit sur notre site.<br /> Un email de confirmation a été
+		envoyé à l'adresse $mail"); 
+	?>
+</body>
+</html>
